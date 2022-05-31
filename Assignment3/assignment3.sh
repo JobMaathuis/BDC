6cec84cfd88401b279eb8ca7ca06309d5c8ecb22
Job Maathuis <j.maathuis@st.hanze.nl>
2022-05-31 10:34:37 +0200

    added assignment3

---
+++ Assignment3/assignment3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time 72:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=17
+#SBATCH --partition=assemblix
+export DATA=/data/dataprocessing/MinIONData/all.fq
+export REF=/data/dataprocessing/MinIONData/all_bacteria.fna
+source /commons/conda/conda_load.sh
+for n in {1..16} ; do /usr/bin/time -o timings3.txt --append -f "${n}\t%e" minimap2  -a -t ${n} $REF $DATA > /dev/null ; done
